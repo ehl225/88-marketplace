f6fd0d151166c1a8dd6b3f2fff34a699e51a700a
Erica <ehlam225@gmail.com>
2021-03-26 13:09:14 -0400

    Update CSS

--- src/views/AboutUs.js
+++ src/views/AboutUs.js
@@ -22,7 +22,7 @@ export default class AboutUs extends React.Component {
                     <p className="store-info overlay-top" id="about-email">{email}</p>
                     <p className="store-info overlay-top" id="about-phone">{phone}</p>
                     </div>
-                    <iframe className="overlay-top" id="google-map" title="google-map" src={googleMapLink} width="350" height="250" frameBorder="0" allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
+                    <iframe className="overlay-top" id="google-map" title="google-map" src={googleMapLink} width="350" height="250" frameBorder="0" style={{border:0}} allowFullScreen="" ariaHidden="false" tabIndex="0"></iframe>
                 </div>
             </div>
         )
